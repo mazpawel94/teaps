3e8cb5762b48141aa280723f353140d55386039f
mazpawel94 <mazpawel94@gmail.com>
2019-01-14 12:14:15 +0100

    addition and validation  new employee

--- teaps.js
+++ teaps.js
@@ -161,13 +161,38 @@ payments: [
         name: 'Ela S.',
         date: '09/01/2019'
     },
-]
-
+],
+newEmployee: '',
 };
     
 new Vue ({
     el: '#app',
     data: data,
+    methods: {
+
+        checkName: function(newName) {
+            let uniqueName = true;
+            this.czarka.forEach(e => {
+                if(e.name.toUpperCase() === newName.toUpperCase())
+                    uniqueName = false;
+            });
+            return uniqueName;
+        },
+        addEmployee: function() {
+            const name = this.newEmployee.trim();
+            if(this.checkName(name)) {
+                this.czarka.push({
+                    name: name,
+                    money: 0
+                });
+                this.newEmployee = '';
+            }
+            else {
+                alert('Istnieje osoba o takim imieniu, dodaj pierwszą literę nazwiska lub inny identyfikujący Cię element');
+            }
+            
+        }
+    },
     computed: {
         width: function() {
             return this.money * 30;
