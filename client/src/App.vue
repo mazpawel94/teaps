32e18c760b13b37b132129a75291d9682997f50f
mazpawel94 <mazpawel94@gmail.com>
2019-05-01 13:48:02 +0200

    block possibility changing before succesful saving to base

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-     <money v-model='coins'/>
-    <employees v-model='todayWorkers' :coins='coins'/>
+    <money v-model="coins"/>
+    <employees v-model="todayWorkers" :coins="coins"/>
     <logbook/>
     <payments/>
-    <tips-form :coins='coins' :todayWorkers='todayWorkers'/>
+    <tips-form :coins="coins" :todayWorkers="todayWorkers"/>
   </div>
 </template>
 
 <script>
-import Money from './components/Money.vue'
-import Employees from './components/Employees.vue'
-import Logbook from './components/Logbook.vue'
-import Payments from './components/Payments.vue'
-import TipsForm from './components/TipsForm.vue'
+import Money from "./components/Money.vue";
+import Employees from "./components/Employees.vue";
+import Logbook from "./components/Logbook.vue";
+import Payments from "./components/Payments.vue";
+import TipsForm from "./components/TipsForm.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
-    return{
+    return {
       todayWorkers: [],
-      coins: [],
-      }
+      coins: []
+    };
   },
   components: {
     Money,
@@ -30,21 +30,29 @@ export default {
     Payments,
     TipsForm
   }
-}
+};
 </script>
 
 <style>
-
-*{
+* {
   margin: 0;
   padding: 0;
 }
+.loader {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(53, 52, 52, 0.548);
+  width: 100vw;
+  height: 100vh;
+  z-index: 99;
+}
 #app {
-    background-image: url(img/money-case-163495_1280.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
-    overflow-x: hidden;
-    height: 100vh;
-     font-family: 'Montserrat', sans-serif;
+  background-image: url(img/money-case-163495_1280.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  overflow-x: hidden;
+  height: 100vh;
+  font-family: "Montserrat", sans-serif;
 }
 </style>
--- client/src/components/TipsForm.vue
+++ client/src/components/TipsForm.vue
@@ -1,63 +1,79 @@
 <template>
-    <div>
-  <div class="tips-form" v-if="visible && (newTips>0 || todayWorkers.length)">
-    <h3 v-if="coins.length">{{beautyAmount(sum() - startAmount)}}</h3>
-    <ul>
-      <li v-for="worker in todayWorkers" v-bind:key="worker.name">
-        <span>{{worker.name}}</span>
-        <select name="hours" v-model="worker.hours">
-          <option value="1">cała zmiana</option>
-          <option value="0.5">pół zmiany</option>
-          <option value="2">podwójna zmiana</option>
-        </select>
-      </li>
-    </ul>
-    <button class="save-changes" @click="newCalculation">Zapisz zmiany</button>
-  </div>    
+  <div>
+    <div class="loader" v-if="successSave>=0"></div>
+    <div class="tips-form" v-if="visible && (newTips>0 || todayWorkers.length)">
+      <h3 v-if="coins.length">{{beautyAmount(sum() - startAmount)}}</h3>
+      <ul>
+        <li v-for="worker in todayWorkers" v-bind:key="worker.name">
+          <span>{{worker.name}}</span>
+          <select name="hours" v-model="worker.hours">
+            <option value="1">cała zmiana</option>
+            <option value="0.5">pół zmiany</option>
+            <option value="2">podwójna zmiana</option>
+          </select>
+        </li>
+      </ul>
+      <button class="save-changes" @click="newCalculation">Zapisz zmiany</button>
+    </div>
     <div class="saved-status" v-if="successSave>=0">
-      <span v-if="successSave<4"> Trwa zapis... <br>{{(successSave/4) * 100}}%</span>
-      <span v-else> Zapis danych zakończony sukcesem!<br><button @click="successSave = -1"> Zamknij</button></span>  
-  </div>
+      <span v-if="successSave<4">
+        Trwa zapis...
+        <br>
+        {{(successSave/4) * 100}}%
+      </span>
+      <span v-else>
+        Zapis danych zakończony sukcesem!
+        <br>
+        <button @click="successSave = -1">Zamknij</button>
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
 import DBService from "../DBService";
 export default {
-    data() {
-        return {
-            startAmount: 0,
-            visible: true,
-            successSave: -1,
-            error: ''
-        }
-    },
+  data() {
+    return {
+      startAmount: 0,
+      visible: true,
+      successSave: -1,
+      error: ""
+    };
+  },
   props: ["coins", "todayWorkers"],
   async created() {
-      try {
-          const a = await DBService.getSum();
-          this.startAmount = a[0].sum;
-      } catch (error) {
-        this.error = error;
-      }
-      
+    try {
+      const a = await DBService.getSum();
+      this.startAmount = a[0].sum;
+    } catch (error) {
+      this.error = error;
+    }
   },
   methods: {
-
     beautyAmount: function(amount) {
-      let sign = '';
-      if(amount<0) sign = '-';
+      let sign = "";
+      if (amount < 0) sign = "-";
       amount = Math.abs(amount);
       return `${sign}${Math.floor(amount / 100)},${
         amount % 100 < 10 ? "0" + (amount % 100) : amount % 100
       } zł`;
     },
 
-    sum: function () {
-      const sum = this.coins[0].amount*1 + this.coins[1].amount*2 + this.coins[2].amount*5 + this.coins[3].amount*10 + this.coins[4].amount*20 + this.coins[5].amount*50 + this.coins[6].amount*100 + this.coins[7].amount*200 + this.coins[8].amount*500 + this.coins[9].amount*1000;
+    sum: function() {
+      const sum =
+        this.coins[0].amount * 1 +
+        this.coins[1].amount * 2 +
+        this.coins[2].amount * 5 +
+        this.coins[3].amount * 10 +
+        this.coins[4].amount * 20 +
+        this.coins[5].amount * 50 +
+        this.coins[6].amount * 100 +
+        this.coins[7].amount * 200 +
+        this.coins[8].amount * 500 +
+        this.coins[9].amount * 1000;
       return sum;
-        
-        },
+    },
 
     currentDate: function() {
       let date = new Date();
@@ -70,8 +86,8 @@ export default {
     },
 
     newCalculation: function() {
-      if(!this.todayWorkers.length) return alert('Zaznacz pracowników');
-      this.visible=false;
+      if (!this.todayWorkers.length) return alert("Zaznacz pracowników");
+      this.visible = false;
       this.successSave++;
       let todayWorkersSum = this.todayWorkers.reduce((e1, e2, index) => {
         if (index === 1) return parseFloat(e1.hours) + parseFloat(e2.hours);
@@ -89,19 +105,23 @@ export default {
         todayMoney.push(e.money);
       });
       this.saveEmployeeToBase(todayName, todayMoney);
-      let participantsTable =[];
+      let participantsTable = [];
       [...this.todayWorkers].forEach(e => {
-          participantsTable.push(e.name)});
-      this.saveSummaryToBase(this.currentDate(), participantsTable.join(', '), this.beautyAmount(this.newTips));
+        participantsTable.push(e.name);
+      });
+      this.saveSummaryToBase(
+        this.currentDate(),
+        participantsTable.join(", "),
+        this.beautyAmount(this.newTips)
+      );
       this.saveSum(this.sum());
       const coinsName = [];
       const coinsAmount = [];
-      [...this.coins].forEach(e=> {
+      [...this.coins].forEach(e => {
         coinsName.push(e.coin);
         coinsAmount.push(e.amount);
-          
       });
-      this.saveToBaseCoin(coinsName, coinsAmount);     
+      this.saveToBaseCoin(coinsName, coinsAmount);
     },
 
     async saveEmployeeToBase(name, money) {
@@ -113,13 +133,13 @@ export default {
       }
     },
 
-    async saveSummaryToBase(date, participants,sum) {
-        try {
-            await DBService.addSummary(date, participants, sum);
-            this.successSave++;
-        } catch (error) {
-          this.error = error;
-        }
+    async saveSummaryToBase(date, participants, sum) {
+      try {
+        await DBService.addSummary(date, participants, sum);
+        this.successSave++;
+      } catch (error) {
+        this.error = error;
+      }
     },
 
     async saveSum(sum) {
@@ -138,15 +158,14 @@ export default {
       } catch (error) {
         this.error = error;
       }
-    },
-
+    }
   },
 
-   computed: {
-          newTips: function() {
-              if(!this.coins.length) return 0;
-            return this.sum() - this.startAmount;
-        }
+  computed: {
+    newTips: function() {
+      if (!this.coins.length) return 0;
+      return this.sum() - this.startAmount;
+    }
   }
 };
 </script>
@@ -214,8 +233,9 @@ export default {
   border: 3px solid rgb(100, 83, 7);
   border-radius: 10px;
   box-shadow: 5px -2px 30px 1px black;
+  z-index: 100;
 }
- .saved-status button {
+.saved-status button {
   cursor: pointer;
   background-color: #094604;
   color: white;
